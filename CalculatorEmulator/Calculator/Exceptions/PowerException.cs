cfda2a6cd5766cb83f9ea9ddcd726cd3d9e0bec2
Alex Carstensen <matthacker.alex@gmail.com>
2015-11-14 16:57:52 +0100

    added Calculator interface and implemented it. added test cases for add and subtract

---
+++ CalculatorEmulator/Calculator/Exceptions/PowerException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Calculator.Exceptions
+{
+    [Serializable]
+    internal class PowerException : Exception
+    {
+        private double exp;
+        private double x;
+
+        public PowerException()
+        {
+        }
+
+        public PowerException(string message) : base(message)
+        {
+        }
+
+        public PowerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public PowerException(double x, double exp)
+        {
+            this.x = x;
+            this.exp = exp;
+        }
+
+        protected PowerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
