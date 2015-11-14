c5cb80f27227ea4ce30eebd45e4bfbf918a9539e
Alex Carstensen <matthacker.alex@gmail.com>
2015-11-14 16:58:16 +0100

    onces more..

---
+++ CalculatorEmulator/Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,104 @@
+﻿using Calculator;
+using NUnit.Framework;
+
+namespace CalculatorUnitTest
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        private ICalculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new CalculatorEmulator();
+        }
+#region Add
+        [Test]
+        public void Add_aIs2bIs2_Return4()
+        {
+            var result = _calculator.Add(2, 2);
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Add_aIs2bIsMinus2_Return0()
+        {
+            var result = _calculator.Add(2, -2);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Add_aIs2bIsMinus3_ReturnMinus1()
+        {
+            var result = _calculator.Add(2, -3);
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Add_aIsMinus2bIs2_Return0()
+        {
+            var result = _calculator.Add(-2, 2);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Add_aIsMinus3bIs2_ReturnMinus1()
+        {
+            var result = _calculator.Add(-3, 2);
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Add_aIsMinus3bIsMinus2_ReturnMinus5()
+        {
+            var result = _calculator.Add(-3, -2);
+            Assert.That(result, Is.EqualTo(-5));
+        }
+        #endregion
+#region Subtract
+        [Test]
+        public void Subtract_aIs2bIs2_Return0()
+        {
+            var result = _calculator.Subtract(2, 2);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Subtract_aIs2bIsMinus2_Return4()
+        {
+            var result = _calculator.Subtract(2, -2);
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Subtract_aIs2bIsMinus3_ReturnMinus5()
+        {
+            var result = _calculator.Subtract(2, -3);
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Subtract_aIsMinus2bIs2_ReturnMinus4()
+        {
+            var result = _calculator.Subtract(-2, 2);
+            Assert.That(result, Is.EqualTo(-4));
+        }
+
+        [Test]
+        public void Subtract_aIsMinus3bIs2_ReturnMinus5()
+        {
+            var result = _calculator.Subtract(-3, 2);
+            Assert.That(result, Is.EqualTo(-5));
+        }
+
+        [Test]
+        public void Subtract_aIsMinus3bIsMinus2_Return5()
+        {
+            var result = _calculator.Subtract(-3, -2);
+            Assert.That(result, Is.EqualTo(-1));
+        }
+#endregion
+
+    }
+}
---
+++ CalculatorEmulator/Calculator/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Calculator.Exceptions;
+
+namespace Calculator
+{
+    /// <summary>
+    /// This class have the following calculation abilities: Sum, Subtract, multiply and Power
+    /// </summary>
+    public class CalculatorEmulator : ICalculator
+    {
+        /// <summary>
+        /// Returns the +sum of two variables
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Returns the - sum of two variables
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Returns the multiplication of two variables
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Returns a number raised to a specific variable. 
+        /// If x or exp is below 0 throw exception
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public double Power(double x, double exp)
+        {
+            if (x >= 0 && exp >= 0)
+                return Math.Pow(x, exp);
+            throw new PowerException(x, exp);
+        }
+    }
+}
